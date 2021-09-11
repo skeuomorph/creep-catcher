09630e1cbaf2ec79f1a917575fdf3c3e66f76e9e
Nathaniel Okenwa <nokenwa@hotmail.co.uk>
2021-09-11 11:57:15 +0100

    Added Menu Toggle Functionality

--- react-app/src/pages/components/navbar.js
+++ react-app/src/pages/components/navbar.js
@@ -1,113 +1,133 @@
-import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import React, { Component, createRef } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Bar = styled.div`
-text-align: center;
-border-bottom: solid black 2px;
-height: 5em;
-`
+  text-align: center;
+  border-bottom: solid black 2px;
+  height: 5em;
+`;
 const Title = styled.h1`
-display: inline-block;
-`
+  display: inline-block;
+`;
 const Menu = styled.nav`
-background-color: red;
-width: 80%;
-position: fixed;
-right: -80%;
-top: 5em;
+  background-color: gray;
+  border: solid 1px black;
+  width: 80%;
+  position: fixed;
+  right: -81%;
+  top: 5.1em;
 
-transition: transform .3s cubic-bezier(0, .52, 0, 1);
+  transition: right 1s cubic-bezier(0, 0.52, 0, 1);
 
-&.show {
+  &.show {
     right: 0;
-}
-`
+  }
+`;
 
 const ToggleMenu = styled.button`
-float: right;
-height: inherit;
-width: 7em;
-border:none;
-background-color: inherit;
+  float: right;
+  height: inherit;
+  width: 7em;
+  border: none;
+  background-color: inherit;
 
-&:hover {
-    background-color: red;
-}
-
-`
+  &:hover {
+    background-color: white;
+  }
+`;
 
 const MenuItems = styled.ul`
-margin:none;
-`
+  margin: 0;
+  padding: 0 0;
+`;
 const MenuItem = styled.li`
-list-style-type: none;
+  list-style-type: none;
+  margin: 0;
+  padding: 0 0;
 
-&:hover {
-    background-color:yellow;
-}
-`
+  &:hover {
+    background-color: white;
+  }
+`;
 const MenuLink = styled(Link)`
- width:7em;
+  width: 7em;
+  margin: 0 0;
+  padding: 0 0;
 
-`
+  text-decoration: none;
+  color: black;
 
-export class Navbar extends Component{
+  & > p {
+    margin: 0 0;
+    line-height: 4em;
+  }
+`;
 
-    constructor(props,context){
-        super(props,context);
-        this.state = {
-            visible: false
-        }
+export class Navbar extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      visible: false,
+    };
+    this.menu = createRef();
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.clicked = this.clicked.bind(this);
+  }
 
-        this.toggleMenu = this.toggleMenu.bind(this);
-        this.clicked = this.clicked.bind(this);
-    }
+  toggleMenu() {
+    console.log("hello");
+    this.menu.current.classList.toggle("show");
+  }
 
-    toggleMenu() {
-        console.log('hello')
-        this.setState({
-            visible:!this.state.visible
-        })
-    }
+  clicked(e) {
+    this.toggleMenu();
+  }
 
-    clicked(e) {
-        this.toggleMenu();
-      }
+  render() {
+    return (
+      <Bar>
+        <Title>Creep</Title>
+        <Title>Catcher</Title>
 
-    render(){
-        
-        return(
-            <Bar>
-                <Title>Creep</Title>
-                <Title>Catcher</Title>
-
-                <ToggleMenu onClick={this.clicked}>burger</ToggleMenu>
-            <Menu>
-                <MenuItems menuVisibility={this.state.visible}>
-                    <MenuItem>
-                        <MenuLink to="/map"><p>Search on Map</p></MenuLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuLink to="/report"><p>Report</p></MenuLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuLink to="/guidelines"><p>Guidelines</p></MenuLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuLink to="/incidents"><p>My Incidents</p></MenuLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuLink to="/support"><p>Support</p></MenuLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuLink to="/contact"><p>Contact</p></MenuLink>
-                    </MenuItem>
-                </MenuItems>
-            </Menu>
-        </Bar>
-        )
-    }
+        <ToggleMenu onClick={this.clicked}>burger</ToggleMenu>
+        <Menu ref={this.menu}>
+          <MenuItems menuVisibility={this.state.visible}>
+            <MenuItem>
+              <MenuLink to="/map">
+                <p>Search on Map</p>
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink to="/report">
+                <p>Report</p>
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink to="/guidelines">
+                <p>Guidelines</p>
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink to="/incidents">
+                <p>My Incidents</p>
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink to="/support">
+                <p>Support</p>
+              </MenuLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuLink to="/contact">
+                <p>Contact</p>
+              </MenuLink>
+            </MenuItem>
+          </MenuItems>
+        </Menu>
+      </Bar>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
